f8a611ff1340c4557612f1b89471474cad6755f0
Oleh-Khomenko <olehhomenko12@gmail.com>
2021-03-15 12:32:43 +0200

    update

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -1,134 +1,129 @@
+import math
 import numpy as np
-from typing import List
-np.set_printoptions(precision=3)
+import random as rnd
+
+m = 5
+y_min, y_max = -2050, -1950
+
+x1_min, x1_max = -25, -5
+x2_min, x2_max = 15, 50
+x1_min_norm, x1_max_norm = -1, 1
+x2_min_norm, x2_max_norm = -1, 1
+
+p_prob = (0.99, 0.98, 0.95, 0.90)
+rkr_table = {2: (1.73, 1.72, 1.71, 1.69),
+             6: (2.16, 2.13, 2.10, 2.00),
+             8: (2.43, 4.37, 2.27, 2.17),
+             10: (2.62, 2.54, 2.41, 2.29),
+             12: (2.75, 2.66, 2.52, 2.39),
+             15: (2.9, 2.8, 2.64, 2.49),
+             20: (3.08, 2.96, 2.78, 2.62)}
+
+matrix_of_y = [[rnd.randint(y_min, y_max) for i in range(m)] for j in range(3)]
+average_y = [sum(matrix_of_y[i][j] for j in range(m)) / m for i in range(3)]
+
+quadric_sigma1 = sum([(j - average_y[0]) ** 2 for j in matrix_of_y[0]]) / m
+quadric_sigma2 = sum([(j - average_y[1]) ** 2 for j in matrix_of_y[1]]) / m
+quadric_sigma3 = sum([(j - average_y[2]) ** 2 for j in matrix_of_y[2]]) / m
+
+teta_sigma = math.sqrt((2 * (2 * m - 2)) / (m * (m - 4)))
+
+Fuv1 = quadric_sigma1 / quadric_sigma2
+Fuv2 = quadric_sigma3 / quadric_sigma1
+Fuv3 = quadric_sigma3 / quadric_sigma2
+
+TetaUV1 = ((m - 2) / m) * Fuv1
+TetaUV2 = ((m - 2) / m) * Fuv2
+TetaUV3 = ((m - 2) / m) * Fuv3
+
+Ruv1 = abs(TetaUV1 - 1) / teta_sigma
+Ruv2 = abs(TetaUV2 - 1) / teta_sigma
+Ruv3 = abs(TetaUV3 - 1) / teta_sigma
+
+mx1 = (-1 + 1 - 1) / 3
+mx2 = (-1 - 1 + 1) / 3
+my = sum(average_y) / 3
+a1 = (1 + 1 + 1) / 3
+a2 = (1 - 1 - 1) / 3
+a3 = (1 + 1 + 1) / 3
+a11 = (-1 * average_y[0] + 1 * average_y[1] - 1 * average_y[2]) / 3
+a22 = (-1 * average_y[0] - 1 * average_y[1] + 1 * average_y[2]) / 3
+
+b0 = np.linalg.det(np.dot([[my, mx1, mx2], [a11, a1, a2], [a22, a2, a3]],
+                          np.linalg.inv([[1, mx1, mx2], [mx1, a1, a2], [mx2, a2, a3]])))
+
+b1 = np.linalg.det(np.dot([[1, my, mx2], [mx1, a11, a2], [mx2, a22, a3]],
+                          np.linalg.inv([[1, mx1, mx2], [mx1, a1, a2], [mx2, a2, a3]])))
+
+b2 = np.linalg.det(np.dot([[1, mx1, my], [mx1, a1, a11], [mx2, a2, a22]],
+                          np.linalg.inv([[1, mx1, mx2], [mx1, a1, a2], [mx2, a2, a3]])))
 
 
-class Experiment:
-    def __init__(self,
-                 X1_range: List[int],
-                 X2_range: List[int],
-                 Y_range: List[int], m: int) -> None:
-        self.Rcritical = {
-            5: 2,
-            6: 2.16,
-            7: 2.3,
-            8: 2.43,
-            9: 2.5
-        }
-        self.X1_range = X1_range
-        self.X2_range = X2_range
-        self.Y_range = Y_range
-
-        self.plan_matrix = np.array(
-            [np.random.randint(*self.X1_range, size=3),
-             np.random.randint(*self.X2_range, size=3)]).T
-
-        self.x0 = [np.mean(self.X1_range), np.mean(self.X2_range)]
-
-        self.norm_matrix = self.make_norm_plan_matrix()
-
-        self.m = m
-
-        self.experiment()
-
-        self.b = self.find_b()
-        self.a = self.find_a()
-
-        self.check_b = self.check_b_koefs()
-        self.check_a = self.check_a_koefs()
-
-    def experiment(self):
-        self.y_matrix = np.random.randint(*self.Y_range, size=(3, self.m))
-        self.y_mean = np.mean(self.y_matrix, axis=1)
-
-        self.y_var = np.var(self.y_matrix, axis=1)
-        self.sigma = np.sqrt((2 * (2 * self.m - 2)) / (self.m * (self.m - 4)))
-
-        if not self.check_r():
-            print(f'\n Дисперсія неоднорідна! Змінимо m={self.m} to m={self.m+1}\n')
-            self.m += 1
-            self.experiment()
-
-    def make_norm_plan_matrix(self) -> np.array:
-        self.N = self.plan_matrix.shape[0]
-        self.k = self.plan_matrix.shape[1]
-
-        interval_of_change = [self.X1_range[1] - self.x0[0],
-                              self.X2_range[1] - self.x0[1]]
-        X_norm = [
-            [(self.plan_matrix[i, j] - self.x0[j]) / interval_of_change[j]
-             for j in range(self.k)]
-            for i in range(self.N)
-        ]
-        return np.array(X_norm)
-
-    def check_r(self) -> bool:
-        for i in range(len(self.y_var)):
-            for j in range(len(self.y_var)):
-                if i > j:
-                    if self.y_var[i] >= self.y_var[j]:
-                        R = (abs((self.m - 2) * self.y_var[i] /
-                             (self.m * self.y_var[j]) - 1) / self.sigma)
-                    else:
-                        R = (abs((self.m - 2) * self.y_var[j] /
-                             (self.m * self.y_var[i]) - 1) / self.sigma)
-                    if R > self.Rcritical[self.m]:
-                        print('Variance isn\'t stable!')
-                        return False
-        return True
-
-    def find_b(self) -> np.array:
-        mx1 = np.mean(self.norm_matrix[:, 0])
-        mx2 = np.mean(self.norm_matrix[:, 1])
-
-        a1 = np.mean(self.norm_matrix[:, 0] ** 2)
-        a2 = np.mean(self.norm_matrix[:, 0] * self.norm_matrix[:, 1])
-        a3 = np.mean(self.norm_matrix[:, 1] ** 2)
-
-        my = np.mean(self.y_mean)
-        a11 = np.mean(self.norm_matrix[:, 0] * self.y_mean)
-        a22 = np.mean(self.norm_matrix[:, 1] * self.y_mean)
-
-        b = np.linalg.solve([[1, mx1, mx2],
-                             [mx1, a1, a2],
-                             [mx2, a2, a3]],
-                            [my, a11, a22])
-        return b
-
-    def find_a(self) -> np.array:
-        delta_x = [abs(self.X1_range[1] - self.X1_range[0]) / 2,
-                   abs(self.X2_range[1] - self.X2_range[0]) / 2]
-        a = [(self.b[0] - self.b[1] * self.x0[0] / delta_x[0] -
-              self.b[2] * self.x0[1] / delta_x[1]),
-             self.b[1] / delta_x[0],
-             self.b[2] / delta_x[1]]
-        return np.array(a)
-
-    def check_b_koefs(self) -> np.array:
-        return np.array([
-            (self.b[0] + np.sum(self.b[1:3] * self.norm_matrix[i]))
-            for i in range(self.N)])
-
-    def check_a_koefs(self) -> np.array:
-        return np.array([
-            (self.a[0] + np.sum(self.a[1:3] * self.plan_matrix[i]))
-            for i in range(self.N)])
-
-    def check_results(self) -> None:
-        print('Матриця планування:\n', self.plan_matrix)
-        print('Нормована матриця:\n', self.norm_matrix)
-        print('Матриця Y:\n', self.y_matrix)
-        print('\nНормовані коефіцієнти:     ', self.b)
-        print('Натуралізовані коефіцієнти:', self.a)
-        print('\nY середнє:                               ', self.y_mean)
-        print('Перевірка нормованих коефіцієнтів:       ', self.check_b)
-        print('Перевірка натуралізованих коефіцієнтів:  ', self.check_a)
+def self_dispersion():
+    M = 0
+    M = min(rkr_table, key=lambda x: abs(x - M))
+    p = 0
+    for ruv in (Ruv1, Ruv2, Ruv3):
+        if ruv > rkr_table[M][0]:
+            return False
+        for rkr in range(len(rkr_table[M])):
+            if ruv < rkr_table[M][rkr]:
+                p = rkr
+    return p_prob[p]
 
 
-if __name__ == '__main__':
-    m = 5
-    X1_range = [-25, -5]
-    X2_range = [15, 50]
-    Y_range = [-2050, -1950]
-    res = Experiment(X1_range, X2_range, Y_range, m)
-    res.check_results()
+def regressionCheck():
+    y_norm1 = round(b0 - b1 - b2, 2)
+    y_norm2 = round(b0 + b1 - b2, 2)
+    y_norm3 = round(b0 - b1 + b2, 2)
+
+    if y_norm1 == average_y[0] and y_norm2 == average_y[1] and y_norm3 == average_y[2]:
+        print("Результат збігається з середніми значеннями y")
+    else:
+        print("Результат НЕ збігається з середніми значеннями y")
+
+
+delta_x1 = math.fabs(x1_max - x1_min) / 2
+delta_x2 = math.fabs(x2_max - x2_min) / 2
+x10 = (x1_max + x1_min) / 2
+x20 = (x2_max + x2_min) / 2
+
+A0 = b0 - b1 * x10 / delta_x1 - b2 * x20 / delta_x2
+A1 = b1 / delta_x1
+A2 = b2 / delta_x2
+
+
+def naturalized_regression(x1, x2):
+    return A0 + A1 * x1 + A2 * x2
+
+
+# output
+for i in range(3):
+    print("Y{}: {}, Average: {}".format(i + 1, matrix_of_y[i], average_y[i]))
+print()
+print("σ² y1:", quadric_sigma1, "\nσ² y2:", quadric_sigma2, "\nσ² y3:", quadric_sigma2)
+print("σθ =", teta_sigma, '\n')
+print("Fuv1 =", Fuv1, "\nFuv2 =", Fuv2, "\nFuv3 =", Fuv3, '\n')
+print("θuv1 =", TetaUV1, "\nθuv2 =", TetaUV2, "\nθuv3 =", TetaUV3, '\n')
+print("Ruv1 =", Ruv1, "\nRuv2 =", Ruv2, "\nRuv3 =", Ruv3, '\n')
+print("Однорідна дисперсія:", self_dispersion(), '\n')
+print("mx1:", mx1, "\nmx2:", mx2, "\nmy:", my, '\n')
+print("a1:", a1, "\na2:", a2, "\na3:", a3)
+print("a11:", a11, "\na22:", a22, '\n')
+print("b0:", b0, "\nb1:", b1, "\nb2:", b2)
+print("Натуралізація коефіцієнтів:")
+print("Δx1:", delta_x1, "\nΔx2:", delta_x2)
+print("x10:", x10, "\nx20:", x20)
+print("a0:", A0, "a1:", A1, "a2:", A2, '\n')
+print("Натуралізоване рівняння регресії:")
+naturReg_Y = [round(naturalized_regression(x1_min, x2_min), 2),
+              round(naturalized_regression(x1_max, x2_min), 2),
+              round(naturalized_regression(x1_min, x2_max), 2)]
+print(naturReg_Y)
+
+if naturReg_Y == average_y:
+    print("Коефіцієнти натуралізованого рівняння регресії вірні")
+else:
+    print("Коефіцієнти натуралізованого рівняння регресії НЕ вірні")
+
+regressionCheck()
